74691feb172618b9209dcb1784a1f08bce777f8d
David Dernovoj <david@dernovoj.com>
2017-09-19 13:35:13 +0100

    Reduced the index.js to the actual lib functionality

--- index.js
+++ index.js
@@ -1,34 +1,6 @@
-const start = Date.now();
-
-const args = require('yargs')
-    .option('queryfile', {
-        alias: 'f',
-        default: null,
-        describe: 'path to a query file, must be used if query is not specified'
-    })
-    .option('query', {
-        alias: 'q',
-        default: null,
-        describe: 'query as string, must be used if queryfile is not specified'
-    })
-    .option('outputfile', {
-        alias: 'o',
-        default: `${Date.now()}.xlsx`,
-        describe: 'path to the output file, if not specified the current timestamp is used in the current working directory'
-    })
-    .option('constr', {
-        alias: 'c',
-        default: null,
-        describe: 'connection string to the postgresql database'
-    })
-    .argv;
-
-const {queryfile, query, outputfile, constr} = args;
-
 const pgp = require('pg-promise')();
 const xlsx = require('xlsx');
 const path = require('path');
-
 
 /**
  * Exports an Excel file for a gievn query
@@ -40,20 +12,22 @@ const path = require('path');
  * @return {Promise}
  */
 const run = async function(qf, q, out, con) {
-    if (!qf && !q) {
-        console.log('You must provide either a query or a queryfile.');
-        process.exit(1);
-    }
+    if (!qf && !q)
+        throw new Error(
+            'pg-to-excel requires either a query or a query file path'
+        );
 
-    if (!con) {
-        console.log('You must provide a connection string');
-        process.exit();
-    }
+    if (!out) throw new Error('pg-to-excel must be called with an output path');
+
+    if (!con)
+        throw new Error(
+            'pg-to-excel cannot be called without a connection string'
+        );
 
     try {
         console.log('Connecting to database');
         // Connect to the databse
-        const db = pgp(constr);
+        const db = pgp(con);
 
         // Load either the queryfile or take the query
         const qry = q || new pgp.QueryFile(path.resolve(process.cwd(), qf));
@@ -62,15 +36,19 @@ const run = async function(qf, q, out, con) {
         const results = await db.query(qry);
 
         if (results.length === 0)
-            console.log('Your query returned 0 results. Skipping the creation of an Excel file.');
+            console.log(
+                'Your query returned 0 results. Skipping the creation of an Excel file.'
+            );
 
         console.log(`Got ${results.length} database results`);
 
         const keys = Object.keys(results[0]);
 
-        const rows = [keys].concat(results.map(res => {
-            return keys.map(key => res[key]);
-        }));
+        const rows = [keys].concat(
+            results.map(res => {
+                return keys.map(key => res[key]);
+            })
+        );
 
         const wb = xlsx.utils.book_new();
 
@@ -79,21 +57,20 @@ const run = async function(qf, q, out, con) {
         wb.SheetNames.push('Sheet1');
         wb.Sheets.Sheet1 = sheet1;
 
-        xlsx.writeFile(wb, outputfile);
+        xlsx.writeFile(wb, out);
 
-        console.log(`Wrote ${outputfile}`);
+        console.log(`Wrote ${out}`);
 
         pgp.end();
 
-        console.log(`Took ${Date.now() - start} ms`);
-
         return out;
     } catch (e) {
-        console.log('An error occured. Please open an issue if you think this is an error with the pg-to-excel project.', e);
+        console.log(
+            'An error occured. Please open an issue if you think this is an error with the pg-to-excel project.',
+            e
+        );
         process.exit(1);
     }
 };
-
-run(queryfile, query, outputfile, constr);
 
 module.exports = run;
